167241429d11ab27dcd24a77116d18b10dca66c5
yuina_1056 <yuina.i.1056@gmail.com>
2018-12-26 21:25:23 +0900

    add TrainInfo_Post.json_judge()

--- rb/lib/TrainInfo_Post.rb
+++ rb/lib/TrainInfo_Post.rb
@@ -2,10 +2,19 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+@TrainName = ""
+@TrainCompany = ""
+
 class TrainInfo_Post
     def get_json
         uri = URI.parse('https://rti-giken.jp/fhc/api/train_tetsudo/delay.json')
         json = Net::HTTP.get(uri)
-        JSON.parse(json)
+        puts JSON.parse(json)
     end
+    def json_judge(name,company)
+        if name == @TrainName && company == @TrainCompany
+            return true
+        end
+    end
+
 end
--- rb/spec/rb/TrainInfo_Post_spec.rb
+++ rb/spec/rb/TrainInfo_Post_spec.rb
@@ -1,6 +1,13 @@
-Rspec.describe 'TrainInfo_Post' do
+RSpec.describe 'TrainInfo_Post' do
+  before do
+    @TrainName = ""
+    @TrainCompany = ""
+    @trainInfo_Post = TrainInfo_Post.new
+  end
   it 'trainInfo_Post get_json' do
-      trainInfo_Post = TrainInfo_Post.new
-      trainInfo_Post.get_json()
+      @trainInfo_Post.get_json()
+  end
+  it 'json_judge' do
+    @trainInfo_Post.json_judge(@TrainName,@TrainCompany)
   end
 end
