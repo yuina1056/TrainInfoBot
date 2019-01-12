d529155884ea19dddb803c410bd8546649edd38b
yuina_1056 <yuina.i.1056@gmail.com>
2019-01-12 17:03:15 +0900

    とりあえず全体取るまで作り始める

--- rb/lib/TrainInfo_Post.rb
+++ rb/lib/TrainInfo_Post.rb
@@ -6,15 +6,25 @@ require 'json'
 @TrainCompany = ""
 
 class TrainInfo_Post
-    def get_json
-        uri = URI.parse('https://rti-giken.jp/fhc/api/train_tetsudo/delay.json')
-        json = Net::HTTP.get(uri)
-        puts JSON.parse(json)
+  #JSON取得
+  def get_json
+    uri = URI.parse('https://rti-giken.jp/fhc/api/train_tetsudo/delay.json')
+    json = Net::HTTP.get(uri)
+    result = JSON.parse(json)
+  end
+  #JSONの中身判定
+  def json_judge(json_obj)
+    for objAllay in json_obj do
+      if @TrainName == objAllay.name && @TrainCompany == objAllay.company
+        return true
+      end  
     end
-    def json_judge(name,company)
-        if name == @TrainName && company == @TrainCompany
-            return true
-        end
-    end
-
+  end
+  #slackにポストする
+  def slack_post
+    
+  end
 end
+
+traininfo = TrainInfo_Post.new
+puts traininfo.get_json
--- rb/spec/rb/TrainInfo_Post_spec.rb
+++ rb/spec/rb/TrainInfo_Post_spec.rb
@@ -1,13 +1,11 @@
 RSpec.describe 'TrainInfo_Post' do
   before do
-    @TrainName = ""
-    @TrainCompany = ""
     @trainInfo_Post = TrainInfo_Post.new
   end
   it 'trainInfo_Post get_json' do
       @trainInfo_Post.get_json()
   end
-  it 'json_judge' do
-    @trainInfo_Post.json_judge(@TrainName,@TrainCompany)
+  it 'slack_post' do
+      @TrainInfo_Post.slack_post()
   end
 end
