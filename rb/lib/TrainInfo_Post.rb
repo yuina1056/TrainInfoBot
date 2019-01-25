816cda6267b9ab399ace24f13367416213614bf8
yuina_1056 <yuina.i.1056@gmail.com>
2019-01-25 23:11:59 +0900

    これで動いたからlambdaに上げた

--- rb/lib/TrainInfo_Post.rb
+++ rb/lib/TrainInfo_Post.rb
@@ -1,10 +1,13 @@
+# -*- encoding: UTF-8 -*-
 require 'net/http'
 require 'uri'
 require 'json'
+require 'date'
 require 'slack-ruby-client'
 require 'dotenv'
 
-train_company = '名古屋鉄道'
+@train_company = '名古屋鉄道'
+@channel_name = 'C9LGQ06LT'
 
 # Slackに電車遅延情報をポストする。
 class Traininfopost
@@ -19,9 +22,13 @@ class Traininfopost
   # 電車遅延が発生しているか判定する
 
   def delay_judge(judge_object, train_company)
-    message = 'no_deley'
+    datetime = Time.now
+    time = datetime.hour.to_s + '時' + datetime.min.to_s + '分現在、'
+    message = time.to_s + train_company.to_s + 'の遅延情報はありません。'
     judge_object.each do |item|
-      message = 'deley' if item['company'].eql?(train_company)
+      if item['company'].eql?(train_company)
+        message = '<@U8U7H4KE0|yuina> ' + time.to_s + train_company.to_s + 'が遅延しています。詳細はこちら→http://top.meitetsu.co.jp/em/'
+      end
     end
     message
   end
@@ -33,21 +40,20 @@ class Traininfopost
       config.token = ENV['SLACK_TOKEN']
     end
     client = Slack::Web::Client.new
-    p client.auth_test
-    client.chat_postMessage(channel: channel, text: message, as_user: true) 
+    client.chat_postMessage(channel: channel, text: message)
   end
 end
 
 def lambda_handler(event:, context:)
   traininfo = Traininfopost.new
   traininfo.slack_post(
-    '#general', traininfo.delay_judge(traininfo.json_get, train_company)
+    'C9LGQ06LT', traininfo.delay_judge(traininfo.json_get, '名古屋鉄道')
   )
-    # TODO: implement
+  # TODO: implement
   { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
 end
 
 traininfo = Traininfopost.new
 traininfo.slack_post(
-  'C9LGQ06LT', traininfo.delay_judge(traininfo.json_get, train_company)
+  @channel_name, traininfo.delay_judge(traininfo.json_get, @train_company)
 )
